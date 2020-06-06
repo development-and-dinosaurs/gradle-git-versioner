522d709a9916875e6a3bc64739ef12ab542e8bfc
Sean O'Toole <seanotoole@mail.com>
2020-06-06 14:55:07 +0100

    Update print version tests
    This uses the previously extracted Gradle and Project objects to perform that
    functionality instead.
    
    Also changes the functions using backticks to a more traditional Java method,
    mainly to appease the recent Sonar scans.

--- src/functionalTest/kotlin/io/toolebox/gradle/gitversioner/tasks/PrintVersionTaskSpec.kt
+++ src/functionalTest/kotlin/io/toolebox/gradle/gitversioner/tasks/PrintVersionTaskSpec.kt
@@ -4,92 +4,68 @@ import io.kotlintest.TestCase
 import io.kotlintest.TestResult
 import io.kotlintest.matchers.string.shouldContain
 import io.kotlintest.specs.FreeSpec
+import io.kotlintest.specs.StringSpec
+import io.toolebox.gradle.gitversioner.util.Gradle
+import io.toolebox.gradle.gitversioner.util.Project
 import java.io.File
 import org.eclipse.jgit.api.Git
 import org.gradle.testkit.runner.GradleRunner
 
-class PrintVersionTaskSpec : FreeSpec() {
-    private val projectDirectory = File("build/tmp/functionalTest/PrintVersionTaskSpec")
+class PrintVersionTaskSpec : StringSpec() {
 
-    override fun beforeTest(testCase: TestCase) {
-        projectDirectory.deleteRecursively()
-        File("src/functionalTest/resources/settings.gradle").copyTo(File(projectDirectory, "settings.gradle"))
-        super.beforeTest(testCase)
-    }
+    private val directory = File("build/tmp/functionalTest/GitVersionerSpec")
+    private var project = Project.createProject(directory)
+    private var gradle = Gradle(directory)
 
     override fun afterTest(testCase: TestCase, result: TestResult) {
-        projectDirectory.deleteRecursively()
+        directory.deleteRecursively()
         super.afterTest(testCase, result)
     }
 
     init {
-        "Version printing" - {
-            "using groovy" - {
-                "prints version using default configuration when none is supplied" {
-                    `given the project is using a groovy build file`("default")
-                    `given the project has a git repository with commits`()
+        "prints version using default configuration when none is supplied in Groovy" {
+            project.withSettingsFile().withGit().withGroovyGradleFile("default")
+            addCommits(project)
 
-                    val result = `when I run the task`("printVersion")
+            val result = gradle.runTask("printVersion")
 
-                    result.output shouldContain "1.1.1.1"
-                }
-                "prints version using provided configuration when supplied" {
-                    `given the project is using a groovy build file`("configured")
-                    `given the project has a git repository with commits`()
+            result.output shouldContain "1.1.1.1"
+        }
+        "prints version using provided configuration when supplied in Groovy" {
+            project.withSettingsFile().withGit().withGroovyGradleFile("configured")
+            addCommits(project)
 
-                    val result = `when I run the task`("printVersion")
+            val result = gradle.runTask("printVersion")
 
-                    result.output shouldContain "2.1.1.4"
-                }
-            }
-            "using kotlin" - {
-                "prints version using default configuration when none is supplied" {
-                    `given the project is using a kotlin build file`("default")
-                    `given the project has a git repository with commits`()
+            result.output shouldContain "2.1.1.4"
+        }
+        "prints version using default configuration when none is supplied in Kotlin" {
+            project.withSettingsFile().withGit().withKotlinGradleFile("default")
+            addCommits(project)
 
-                    val result = `when I run the task`("printVersion")
+            val result = gradle.runTask("printVersion")
 
-                    result.output shouldContain "1.1.1.1"
-                }
-                "prints version using provided configuration when supplied" {
-                    `given the project is using a kotlin build file`("configured")
-                    `given the project has a git repository with commits`()
+            result.output shouldContain "1.1.1.1"
+        }
+        "prints version using provided configuration when supplied in Kotlin" {
+            project.withSettingsFile().withGit().withKotlinGradleFile("configured")
+            addCommits(project)
 
-                    val result = `when I run the task`("printVersion")
+            val result = gradle.runTask("printVersion")
 
-                    result.output shouldContain "2.1.1.4"
-                }
-            }
+            result.output shouldContain "2.1.1.4"
         }
     }
 
-    private fun `given the project has a git repository with commits`() {
-        val repo = Git.init().setDirectory(projectDirectory).call()
-        commit(repo, "trex")
-        commit(repo, "stego")
-        commit(repo, "compy")
-        commit(repo, "[major]")
-        commit(repo, "[minor]")
-        commit(repo, "[patch]")
-        commit(repo, "message")
+    private fun addCommits(project: Project) {
+        project
+            .withCommit("trex")
+            .withCommit("stego")
+            .withCommit("compy")
+            .withCommit("[major]")
+            .withCommit("[minor]")
+            .withCommit("[patch]")
+            .withCommit("message")
     }
 
-    private fun commit(repo: Git, message: String) {
-        repo.commit().setAllowEmpty(true).setMessage(message).call()
-    }
-
-    private fun `given the project is using a groovy build file`(name: String) {
-        File("src/functionalTest/resources/$name-build.gradle").copyTo(File(projectDirectory, "build.gradle"))
-    }
-
-    private fun `given the project is using a kotlin build file`(name: String) {
-        File("src/functionalTest/resources/$name-build.gradle.kts").copyTo(File(projectDirectory, "build.gradle.kts"))
-    }
-
-    private fun `when I run the task`(task: String) = GradleRunner.create()
-            .withProjectDir(projectDirectory)
-            .withArguments(task, "-q")
-            .withPluginClasspath()
-            .forwardOutput()
-            .build()
 }
